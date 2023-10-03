d54b5d369421f0a4c54cb3c0f49215e2bab345b2
Ale Primo <alejandrojuliancarullo@hotmail.com>
2023-10-03 19:59:34 -0300

    Correcion pacienteData
    Solucionado el error de ingreso de paciente

--- src/prototiponutris/PrototipoNutris.java
+++ src/prototiponutris/PrototipoNutris.java
@@ -10,10 +10,11 @@ public class PrototipoNutris {
    
     public static void main(String[] args) {
         
-        Paciente prueba1=new Paciente(1.80, "Carasa 987 ", "4612-1866", "Bolaño","Roberto", 5578967, "1547894215","petruza@yahoo.com.ar");
+        //Paciente prueba1=new Paciente(1.80, "Carasa 987 ", "4612-1866", "Bolaño","Roberto", 5578967, "1547894215","petruza@yahoo.com.ar");
         PacienteData pac=new PacienteData();
-        pac.ingresarPaciente(prueba1);
-     
+        //pac.ingresarPaciente(prueba1);
+        //Paciente prueba2=new Paciente(1.53, "Carasa 2500","", "Perez","Juan", 18974532, "","");
+    // pac.ingresarPaciente(prueba2);
     }
     
 }

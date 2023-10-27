589bfd0bf352597bdecf2c3c306b37f20576c633
Ale Primo <alejandrojuliancarullo@hotmail.com>
2023-10-27 19:51:38 -0300

    Script historial

---
+++ scriptBaseDeDatos/nutris.historial.sql
@@ -0,0 +1,26 @@
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(1,9,5,'200.00', '2020-08-08' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(2,9,5,'195.40', '2021-01-08' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(3,9,5,'190.38', '2021-10-03' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(4,8,2,'100.12', '2021-12-05' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(5,8,2,'106.02', '2021-06-05' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(6,9,5,'193.67', '2021-06-08' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(7,8,2,'103.02', '2022-06-09' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(8,8,2,'95.22', '2022-12-05' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(9,40,1,'87.44', '2022-12-23' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(10,40,1,'92.41', '2022-12-25' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(11,40,1,'97.84', '2022-12-28' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(12,40,1,'105.24', '2022-12-30' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(13,8,2,'90.93', '2023-03-08' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(14,39,4,'56.95', '2023-08-09' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(15,39,4,'56.05', '2023-08-11' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(16,39,4,'54.90', '2023-08-14' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(17,39,4,'54.67', '2023-08-16' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(18,4,3,'99.64', '2023-08-28' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(19,4,3,'90.60', '2023-08-30' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(20,4,3,'82.74', '2023-09-02' );
+insert into nutris.historial (idHistorial, idPaciente, idDieta, pesoActual, fechaRegistro) values(21,4,3,'78.33', '2023-09-04' );
+
+
+
+
+
